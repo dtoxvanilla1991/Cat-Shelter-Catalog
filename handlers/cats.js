da2c57677b81b07076ee61878132b47d36805973
Yuri Avdijevski <dtoxvanilla@gmail.com>
2021-12-14 22:51:46 +0000

    fixed rename FS uploads issue with formidable(cross-link)

--- handlers/cats.js
+++ handlers/cats.js
@@ -6,114 +6,124 @@ const cats = require('../data/cats.json')
 const breeds = require('../data/breeds.json')
 
 
-module.exports = (req, res)=>{
+module.exports = (req, res) => {
 
   const pathname = url.parse(req.url).pathname;
 
   console.log(pathname)
-  if(pathname==='/cats/add-cat' && req.method ==='GET'){
+  if (pathname === '/cats/add-cat' && req.method === 'GET') {
     // Load the add form 
 
 
-    let filePath =path.normalize(path.join( __dirname, '../views/addCat.html'))
+    let filePath = path.normalize(path.join(__dirname, '../views/addCat.html'))
     const index = fs.createReadStream(filePath);
 
-    index.on('data', (data)=>{
-      let catBreedPlaceholder = breeds.map(breed=> `<option value="${breed}"> ${breed}</option>`).join('\n');
+    index.on('data', (data) => {
+      let catBreedPlaceholder = breeds.map(breed => `<option value="${breed}"> ${breed}</option>`).join('\n');
       let modifiedData = data.toString().replace('{{catBreeds}}', catBreedPlaceholder)
       res.write(modifiedData);
       res.end();
     })
 
-    index.on('error', (err)=>{
+    index.on('error', (err) => {
       console.log(err);
     })
 
-  }else if(pathname==='/cats/add-cat' && req.method ==='POST'){
+  } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
     // Process adding a cat
 
     let form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files)=>{
+    form.parse(req, (err, fields, files) => {
 
-      if(err) throw err;
+      if (err) throw err;
 
       let filename;
-      if(files.upload.name){
-  
-        let oldPath = files.upload.path; 
-        let newPath = path.normalize(path.join(__dirname, '../content/images/'+ files.upload.name))
 
-        fs.rename(oldPath, newPath, (err)=>{
+      if (files.upload.name) {
+
+        let oldPath = files.upload.path;
+        let newPath = path.normalize(path.join(__dirname, '../content/images/' + files.upload.name));
+        console.log(oldPath + '   WE ARE HERE!!!');
+        console.log(newPath + '   WE ARE HERE AGAINNNN!!!');
+        fs.copyFile(oldPath, newPath, (err) => {
           if (err) throw err;
           console.log('File was uploaded successfuly');
-        })
-        filename = files.upload.name
-      }else{
-        filename = 'no-image.png'
+        });
+        filename = files.upload.name;
+      } else {
+        filename = 'no-image.png';
       }
       console.log(fields);
 
-      cats.push({id: Date.now(), ...fields, image:filename})
+      cats.push({
+        id: Date.now(),
+        ...fields,
+        image: filename
+      });
       let catsJson = JSON.stringify(cats);
-      fs.writeFile('./data/cats.json', catsJson, ()=>{
-        res.writeHead(301, {location:'/'})
+      fs.writeFile('./data/cats.json', catsJson, () => {
+        res.writeHead(301, {
+          location: '/'
+        });
         res.end();
       })
 
     })
 
-  }else if(pathname==='/cats/add-breed' && req.method==='GET'){
+  } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
 
     // Load the add breed form 
 
-    let filePath =path.normalize(path.join( __dirname, '../views/addBreed.html'))
+    let filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'))
     const index = fs.createReadStream(filePath);
 
     index.pipe(res);
-  }else if(pathname==='/cats/add-breed' && req.method==='POST'){
+  } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
     // Process adding a breed
 
     let form = new formidable.IncomingForm();
 
-    form.parse(req, (err, fields, files)=>{
-      if(err) throw err;
+    form.parse(req, (err, fields, files) => {
+      if (err) throw err;
 
       breeds.push(fields.breed);
       let breedsJson = JSON.stringify(breeds);
-      fs.writeFile('./data/breeds.json', breedsJson, 'utf-8', ()=>{
+      fs.writeFile('./data/breeds.json', breedsJson, 'utf-8', () => {
         console.log('The breed was added successfully')
       })
 
-      res.writeHead(301, {location:'/'})
+      res.writeHead(301, {
+        location: '/'
+      })
       res.end()
 
     });
 
 
-  }else if(pathname.includes('/cats-edit') && req.method=='GET'){
+  } else if (pathname.includes('/cats-edit') && req.method == 'GET') {
     // LOAD THE EDIT PAGE 
 
-    const urlArr = pathname.split('/'); 
-    const catId = urlArr[urlArr.length-1];
+    const urlArr = pathname.split('/');
+    const catId = urlArr[urlArr.length - 1];
     let filePath = path.normalize(path.join(__dirname, '../views/editCat.html'));
 
-    let currentCat = cats.find(c=> c.id ==catId)
+    let currentCat = cats.find(c => c.id == catId)
     const index = fs.createReadStream(filePath);
 
-    index.on('data', (data)=>{
+    index.on('data', (data) => {
 
       let modifiedData = data.toString().replace('{{id}}', catId)
       modifiedData = modifiedData.replace('{{name}}', currentCat.name)
       modifiedData = modifiedData.replace('{{description}}', currentCat.description);
 
-      const breedsAsOptions = breeds.map(breed=> {
+      const breedsAsOptions = breeds.map(breed => {
         console.log(currentCat.breed, breed)
-        if(breed=== currentCat.breed){
-        
-            return `<option value="${breed}" selected> ${breed}</option>`
-          }else{
-            return `<option value="${breed}"> ${breed}</option>`
-          }
+        if (breed === currentCat.breed) {
+
+          return `<option value="${breed}" selected> ${breed}</option>`
+        } else {
+          return `<option value="${breed}"> ${breed}</option>`
+        }
       }).join()
 
       modifiedData = modifiedData.replace('{{catBreeds}}', breedsAsOptions);
@@ -124,92 +134,100 @@ module.exports = (req, res)=>{
 
     })
 
-  }else if(pathname.includes('/cats-edit') && req.method=='POST'){
+  } else if (pathname.includes('/cats-edit') && req.method == 'POST') {
     //  PROCESS THE EDIT 
-    const urlArr = pathname.split('/'); 
-    const catId = urlArr[urlArr.length-1];
-    let currentCat = cats.find(c=> c.id ==catId)
+    const urlArr = pathname.split('/');
+    const catId = urlArr[urlArr.length - 1];
+    let currentCat = cats.find(c => c.id == catId)
     let form = new formidable.IncomingForm();
 
-    let filename; 
-    form.parse(req, (err, fields, files)=>{
+    let filename;
+    form.parse(req, (err, fields, files) => {
 
 
-      if(err) throw err;
+      if (err) throw err;
 
-      if(files.upload.name){
-  
-        let oldPath = files.upload.path; 
-        let newPath = path.normalize(path.join(__dirname, '../content/images/'+ files.upload.name))
+      if (files.upload.name) {
 
-        fs.rename(oldPath, newPath, (err)=>{
+        let oldPath = files.upload.path;
+        console.log(oldPath)
+        let newPath = path.normalize(path.join(__dirname, '../content/images/' + files.upload.name))
+        console.log(newPath)
+        fs.rename(oldPath, newPath, (err) => {
           if (err) throw err;
           console.log('File was uploaded successfuly');
         })
         filename = files.upload.name
-       }else{
-          filename = currentCat.image
-        }
-        let allCats = cats.filter(cat=> cat.id != catId)
+      } else {
+        filename = currentCat.image
+      }
+      let allCats = cats.filter(cat => cat.id != catId)
 
-        allCats.push({id:currentCat.id, ...fields, image:filename})
-        let catsJson = JSON.stringify(allCats);
+      allCats.push({
+        id: currentCat.id,
+        ...fields,
+        image: filename
+      })
+      let catsJson = JSON.stringify(allCats);
 
-        fs.writeFile('./data/cats.json', catsJson, ()=>{
-          res.writeHead(301, {location:'/'})
-          res.end();
-        }) 
-        
+      fs.writeFile('./data/cats.json', catsJson, () => {
+        res.writeHead(301, {
+          location: '/'
+        })
+        res.end();
+      })
+
 
     })
 
-  }else if(pathname.includes('/cat-find-new-home') && req.method=='GET'){
+  } else if (pathname.includes('/cat-find-new-home') && req.method == 'GET') {
     // LOAD THE DELETE PAGE 
 
-    const urlArr = pathname.split('/'); 
-    const catId = urlArr[urlArr.length-1];
+    const urlArr = pathname.split('/');
+    const catId = urlArr[urlArr.length - 1];
     let filePath = path.normalize(path.join(__dirname, '../views/deleteCat.html'));
 
-    let currentCat = cats.find(c=> c.id ==catId)
+    let currentCat = cats.find(c => c.id == catId)
 
-    if(currentCat){
+    if (currentCat) {
       const index = fs.createReadStream(filePath);
 
-      index.on('data', (data)=>{
-  
+      index.on('data', (data) => {
+
         let modifiedData = data.toString().replace('{{id}}', catId)
         modifiedData = modifiedData.replace('{{name}}', currentCat.name)
         modifiedData = modifiedData.replace('{{alt}}', currentCat.name)
         modifiedData = modifiedData.replace('{{description}}', currentCat.description);
-        modifiedData = modifiedData.replace('{{image}}', '/content/images/' +currentCat.image);
+        modifiedData = modifiedData.replace('{{image}}', '/content/images/' + currentCat.image);
         modifiedData = modifiedData.replace('{{breed}}', currentCat.breed);
         res.write(modifiedData)
         res.end()
 
-      })  
+      })
     }
 
-  }else if(pathname.includes('/cats/cat-find-new-home') && req.method=='POST'){
+  } else if (pathname.includes('/cats/cat-find-new-home') && req.method == 'POST') {
     // PROCESS THE DELETE 
 
-    const urlArr = pathname.split('/'); 
-    const catId = urlArr[urlArr.length-1];
+    const urlArr = pathname.split('/');
+    const catId = urlArr[urlArr.length - 1];
 
-    fs.readFile('./data/cats.json', 'utf-8', (err, cats)=>{
-      if(err) throw err
+    fs.readFile('./data/cats.json', 'utf-8', (err, cats) => {
+      if (err) throw err
 
       cats = JSON.parse(cats);
-    
+
       const updatedCates = cats.filter(cat => cat.id != catId);
       let catsJson = JSON.stringify(updatedCates);
-      fs.writeFile('./data/cats.json', catsJson, ()=>{
-        res.writeHead(301, {location:'/'})
+      fs.writeFile('./data/cats.json', catsJson, () => {
+        res.writeHead(301, {
+          location: '/'
+        })
         res.end();
       })
     })
 
-  }
-  else{
+  } else {
     return true
   }
 
--- handlers/home.js
+++ handlers/home.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const qs = require('querystring')
 
+function isObjectEmpty(arr) {
+    var isEmpty = true;
+    for (let item of arr) {
+        isEmpty = false;
+        break; // exiting since we found that the object is not empty
+    }
+    return isEmpty;
+}
+
 module.exports = (req, res) => {
 
     const pathname = url.parse(req.url).pathname;
@@ -10,45 +19,62 @@ module.exports = (req, res) => {
         let filePath = path.join(__dirname, '../views/home/index.html');
 
         // Extracting the url query 
-        let {query}= url.parse(req.url, true)
+        let {
+            query
+        } = url.parse(req.url, true)
 
         fs.readFile(filePath, (err, data) => {
             if (err) {
                 console.log(err);
-                res.writeHead(404, { 'Content-Type': 'text/plain' })
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                })
                 res.write('404: cannot read file');
                 res.end();
                 return;
             }
-            
 
-            fs.readFile('./data/cats.json', 'utf-8', (err, catsData)=>{
-                if(err) throw err;
+
+            fs.readFile('./data/cats.json', 'utf-8', (err, catsData) => {
+                if (err) throw err;
 
                 catsData = JSON.parse(catsData);
 
                 let modifiedCats;
 
-                if(query.search){
-                    // search mode
-                    catsData = catsData.filter(cat=> cat.name.toLowerCase().includes(query.search.toLowerCase()) 
-                    || cat.breed.toLowerCase().includes(query.search.toLowerCase()))
-                    console.log(catsData)
-                    if(catsData.length==0){
-                        modifiedCats = `<p> No cats found</p>`
-                        modifiedData = data.toString().replace('{{cats}}', modifiedCats)
-                        res.writeHead(200, { 'Content-Type': 'text/html' })
-                        res.write(modifiedData);
-                        res.end();
+                if (isObjectEmpty(catsData) == true) {
+                    modifiedCats = `<p> No cats found</p>`
+                    modifiedData = data.toString().replace('{{cats}}', modifiedCats);
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    res.write(modifiedData);
+                    res.end();
 
-                        return;
+                    return;
+                } else {
+                    if (query.search) {
+                        // search mode
+                        catsData = catsData.filter(cat => cat.name.toLowerCase().includes(query.search.toLowerCase()) ||
+                            cat.breed.toLowerCase().includes(query.search.toLowerCase()))
+                        if (catsData.length == 0) {
+                            modifiedCats = `<p> No cats found</p>`
+                            modifiedData = data.toString().replace('{{cats}}', modifiedCats);
+                            res.writeHead(200, {
+                                'Content-Type': 'text/html'
+                            });
+                            res.write(modifiedData);
+                            res.end();
+
+                            return;
+                        }
+
                     }
-              
-                }
 
-                // Home page 
-                modifiedCats = catsData.map(cat =>
-                    ` <li>
+                    // Home page
+
+                    modifiedCats = catsData.map(cat =>
+                        ` <li>
                     <img src="${'./content/images/'+ cat.image}" alt="${cat.name}">
                     <h3>${cat.name}</h3>
                     <p><span>Breed: </span>${cat.breed}</p>
@@ -58,17 +84,20 @@ module.exports = (req, res) => {
                         <li class="btn delete"><a href="/cats/cat-find-new-home/${cat.id}">New Home</a></li>
                     </ul>
                 </li>`
-    
-                )
 
-            modifiedData = data.toString().replace('{{cats}}', modifiedCats.join(''))
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.write(modifiedData);
-            res.end();
+                    );
 
-            })
-              
-        })
+                    modifiedData = data.toString().replace('{{cats}}', modifiedCats.join(''))
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    res.write(modifiedData);
+                    res.end();
+
+                }
+            });
+
+        });
 
     } else {
         return true
